ed66c7ff926215affe246da7c4822438c23f3de0
barnabywalker <barnaby.walker08@gmail.com>
2022-09-14 10:25:59 +0100

    added feature extraction script

--- delimitation_pipeline/datasets/__init__.py
+++ delimitation_pipeline/datasets/__init__.py
@@ -1,7 +1,9 @@
 from .half_earth_dataset import HalfEarthDataset
 from .half_earth_module import HalfEarthModule
+from .specimen_dataset import SpecimenDataset
 
 __all__ = [
     "HalfEarthDataset",
-    "HalfEarthModule"
+    "HalfEarthModule",
+    "SpecimenDataset"
 ]
